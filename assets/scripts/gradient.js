42c7bacb0c7aa72d3295012d3a0cc27417a96f57
Dudi041110 <adam.iqran@gmail.com>
2026-08-07 21:40:41 +0400

    Updating the home page

--- assets/scripts/gradient.js
+++ assets/scripts/gradient.js
@@ -1,25 +1,29 @@
 const about = document.getElementById("about");
 const work = document.getElementById("work");
 const contact = document.getElementById("contact");
+const banner = document.getElementById("banner");
 
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
     let height = document.body.scrollHeight - window.innerHeight;
     let progress = scroll / height;
 
-    if (progress < 0.33) {
+    if (progress < 0.25) {
         about.style.backgroundColor = "#ede7d9";
         work.style.backgroundColor = "#ede7d9";
         contact.style.backgroundColor = "#ede7d9";
+        banner.style.backgroundColor = "#ede7d9";
     } 
-    else if (progress < 0.66) {
+    else if (progress < 0.75) {
         about.style.backgroundColor = "white";
         work.style.backgroundColor = "white";
         contact.style.backgroundColor = "white";
+        banner.style.backgroundColor = "white";
     } 
     else {
         about.style.backgroundColor = "#e9decd";
         work.style.backgroundColor = "#e9decd";
         contact.style.backgroundColor = "#e9decd";
+        banner.style.backgroundColor = "#e9decd";
     }
 });
