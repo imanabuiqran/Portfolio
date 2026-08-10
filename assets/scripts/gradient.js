b69379a35777f200fd2df154466e4183f177ec2c
Dudi041110 <adam.iqran@gmail.com>
2026-08-10 17:14:10 +0400

    better design on the home page

--- assets/scripts/gradient.js
+++ assets/scripts/gradient.js
@@ -3,6 +3,35 @@ const work = document.getElementById("work");
 const contact = document.getElementById("contact");
 const banner = document.getElementById("banner");
 
+let scroll = window.scrollY;
+let height = document.body.scrollHeight - window.innerHeight;
+let progress = scroll / height;
+
+if (progress < 0.25) {
+    about.style.backgroundColor = "white";
+    work.style.backgroundColor = "white";
+    contact.style.backgroundColor = "white";
+    banner.style.backgroundColor = "white";
+} 
+else if (progress < 0.5) {
+    about.style.backgroundColor = "white";
+    work.style.backgroundColor = "white";
+    contact.style.backgroundColor = "white";
+    banner.style.backgroundColor = "white";        
+}
+else if (progress < 0.75) {
+    about.style.backgroundColor = "white";
+    work.style.backgroundColor = "white";
+    contact.style.backgroundColor = "white";
+    banner.style.backgroundColor = "white";
+} 
+else {
+    about.style.backgroundColor = "#e9decd";
+    work.style.backgroundColor = "#e9decd";
+    contact.style.backgroundColor = "#e9decd";
+    banner.style.backgroundColor = "#e9decd";
+}
+
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
     let height = document.body.scrollHeight - window.innerHeight;
